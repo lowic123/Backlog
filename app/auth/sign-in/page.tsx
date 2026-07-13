b79ac378d4de0f5099a1ce480b7d28e12e4332c0
lowic123 <loic.alexis@outlook.com>
2026-07-13 08:22:14 -0400

    remove auth-form file will refactor later

--- app/auth/sign-in/page.tsx
+++ app/auth/sign-in/page.tsx
@@ -1,6 +1,5 @@
 ﻿'use client'
 import { useState } from 'react'
-import AuthForm from '@/components/auth-form'
 import {signInWithEmail } from '@/actions/auth'
 import { Button } from "@/components/ui/button"
 
